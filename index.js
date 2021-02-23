82fb7d4a79678fc6d52c7b4af05d6f86c328d2a4
AgustD <62168083+mbahagus@users.noreply.github.com>
2021-02-23 11:55:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const logger = require("morgan");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const cookieParser = require("cookie-parser");
 const api = require("./router");
+
+app.use(cors());
+app.use(cookieParser());
+app.use(logger('dev'));
+app.use(express.json());
+app.set("json spaces", 2);
 
 app.use("/", api);
 
